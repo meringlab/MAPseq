252e549ecca9f2fa7839c4171cda353088e0a667
PatschD <david.patsch@gmx.at>
2024-04-30 09:08:24 +0200

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,9 @@
 
 wget http://www.microbeatlas.org/mapref/mapref-3.0.full.tar.gz
 # git-lfs pull
+mv mapref-3.0.full.tar.gz data/mapref-3.0.tar.gz
 tar -xvzf data/mapref-3.0.tar.gz && mv mapseq/* data/ && rmdir mapseq && touch data/mapref-3.0.fna
+rm -rf data/mapref-3.0.tar.gz
 
 if [ ! -d "libs/eutils" ]; then
 	mkdir -p libs
